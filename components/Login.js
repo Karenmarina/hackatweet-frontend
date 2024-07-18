79a51f93844fe51e6aa3cf6bbe2211d4c0a9c96a
Manu <manuelmanrique6@hotmail.com>
2024-07-18 17:51:27 +0200

    mise a jour Login front end

--- components/Login.js
+++ components/Login.js
@@ -1,12 +1,27 @@
-import styles from '../styles/Home.module.css';
+import styles from '../styles/Login.module.css';
+import Image from 'next/image';
 
 function Login() {
   return (
     <div>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Coucou <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <div className={styles.containerLeft}>
+      <Image className={styles.logoLeft}  src="/logotwitter.png" alt="logotwitter"
+      width={300}
+      height={300} />
+        </div>
+        <div className={styles.containerRight}>
+            <div className={styles.logoContainer}>
+              <Image src="/logotwitter.png" alt="logotwitter"
+              width={80}
+              height={80}/>
+            </div>
+            <h1 className={styles.h1}>See what's happening</h1>
+            <h2 className={styles.h2}>Join Hackatweet today.</h2>
+            <button className={styles.buttonSignUp}>Sign up</button>
+            <h3 className={styles.h3}>Already have an account ?</h3>
+            <button className={styles.buttonSignIn}>Sign In</button>
+        </div>
       </main>
     </div>
   );
